9fc4d7f086a83e016b5594a9048bdd1a8ac82e84
dilaver <mdilaver@live.com>
2015-03-23 21:40:41 +0200

    site yolu değişti.

--- Mysite/wsgi.py
+++ Mysite/wsgi.py
@@ -1,8 +1,8 @@
 import os,sys
 sys.path.append('/var/www')
-sys.path.append('/var/www/anket')
+sys.path.append('/var/www/Mysite')
  
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "anket.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Mysite.settings")
  
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
